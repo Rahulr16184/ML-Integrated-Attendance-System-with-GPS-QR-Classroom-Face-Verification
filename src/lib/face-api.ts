ed9deba39dfc8e9681ca6cb6818a6e5135adc351
TRACEIN <tracein0@gmail.com>
2025-09-21 16:39:13 +0000

    Ok set up the faceapi and mobile net model

---
+++ src/lib/face-api.ts
@@ -0,0 +1,29 @@
+import * as faceapi from 'face-api.js';
+
+const MODEL_URL = '/models';
+
+let modelsLoaded = false;
+
+export async function loadModels() {
+  if (modelsLoaded) {
+    return;
+  }
+  try {
+    await Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+      faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+      faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+      faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL),
+      faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
+    ]);
+    modelsLoaded = true;
+    console.log('Face-api models loaded successfully');
+  } catch (error) {
+    console.error('Error loading face-api models:', error);
+  }
+}
+
+export function getFaceApi() {
+    return faceapi;
+}
