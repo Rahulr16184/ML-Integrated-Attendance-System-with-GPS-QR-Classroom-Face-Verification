76ef1da18bff2bb7904047c68404faa82569e905
TRACEIN <tracein0@gmail.com>
2025-09-21 16:53:31 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https.://api.qrserver.com',
         hostname: 'api.qrserver.com',
         port: '',
         pathname: '/**',
