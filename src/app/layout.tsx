37d6af31db87b9e406a357b52845edcd78437ee2
TRACEIN <tracein0@gmail.com>
2025-09-29 13:35:07 +0000

    the css is not loaded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
-import { Header } from "@/components/header";
-import { Bot } from "lucide-react";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -25,9 +22,6 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossOrigin=""/>
-        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-          crossOrigin=""></script>
       </head>
       <body className={`${inter.variable} font-body antialiased`}>
         <ThemeProvider
@@ -39,6 +33,9 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+          crossOrigin=""></script>
       </body>
     </html>
   );
